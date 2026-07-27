dbfa96a6c9eaa33bd1c4b23eda3f5216ed3b1aee
Claire <cameron.liam.yon@gmail.com>
2026-07-27 13:16:44 -0400

    created the functions that dynamically create and edit the pizza image based on the users choices (neither are finished, just started)

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // Element references
 const footer = document.querySelector("footer");
+const imageDiv = document.querySelector("#image");
 
 // Function to dynamically add my name and student id to the document
 function createFooter() {
@@ -12,3 +13,25 @@ function createFooter() {
 
 // Run function
 createFooter();
+
+// Function creates the image that swaps when the user selects options
+function createPizzaImage() {
+    let img = document.createElement("img");
+    let url = "img/animator-pizzaria.svg";
+    img.src = url;
+    img.id = "pizza";
+    img.height = 200;
+    img.width = 200;
+    imageDiv.appendChild(img);
+}
+
+// Function changes the image based on the user's choices
+function setPizzaImage() {
+    const img = document.querySelector("#pizza");
+    //  TODO: Get all user selections and change pizza accordingly
+    let url = "img/pepperoni-pizza.svg";
+    img.src = url;
+}
+
+createPizzaImage();
+setPizzaImage();
